b01f56baa7e1c8d482655eb3d4404bb0942f8a2d
abo7adeed <ahmedabohadeed58@gmail.com>
2026-04-26 15:04:08 +0300

    refactor: restructure project files and update environment configurations

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI # type: ignore
 from dotenv import load_dotenv # type: ignore
 load_dotenv(".env")
-from routes import base
+from src.routes import base
 app =FastAPI()
 app.include_router(base.base_router)
